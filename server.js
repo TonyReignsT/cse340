f5fc4096c2a176ac255d78233b7455d6ef9336f0
TonyReignsT <tonyreigns00@gmail.com>
2025-11-15 06:38:16 +0300

    Inventory delivery by classification

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -25,6 +26,8 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 // Index route
 // app.get("/", function(req, res) {
